ff16a1c279e1562d6089031f3d5539fcbbb28805
fadedacr <47284896+fadedacr@users.noreply.github.com>
2020-01-04 18:17:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,9 +88,9 @@ client.on('message', message => {
     }
 });
 
-//setInterval(sendmessage, 15000);
+setInterval(sendmessage, 15000);
 sendrequest() // at init so it shows the player count for the first 100 seconds
-setInterval(sendrequest, 100000)
+setInterval(sendrequest, 100000);
 setInterval(updatename, 10000);
 setInterval(updateplayers, 4600);
 
